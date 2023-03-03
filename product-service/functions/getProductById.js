e5e9dc98778f31ea97bde5613867f68e5eaf46d1
Elena Kim <elena.kim3011@gmail.com>
2023-03-03 23:23:51 +0100

    task 4: log request events; handle errors

--- product-service/functions/getProductById.js
+++ product-service/functions/getProductById.js
@@ -1,29 +1,30 @@
+import middy from "@middy/core";
+
+import { logRequest, handleError } from "../middleware/index.js";
 import productsRepository from "../shared/productsRepository.js";
 
-export default async function getProductById(event) {
-  try {
-    const product = await productsRepository.getProductById(
-      event.pathParameters.productId
-    );
+async function getProductByIdHandler(event) {
+  const product = await productsRepository.getProductById(
+    event.pathParameters.productId
+  );
 
-    if (product) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(product),
-        headers: {
-          "content-type": "application/json",
-        },
-      };
-    } else {
-      return {
-        statusCode: 404,
-        body: "Product not found",
-      };
-    }
-  } catch (error) {
+  if (product) {
     return {
-      statusCode: 500,
-      body: error.message,
+      statusCode: 200,
+      body: JSON.stringify(product),
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: "Product not found",
     };
   }
 }
+
+const getProductById = middy(getProductByIdHandler);
+getProductById.use(logRequest()).use(handleError());
+
+export default getProductById;
--- product-service/functions/getProductsList.js
+++ product-service/functions/getProductsList.js
@@ -1,20 +1,21 @@
+import middy from "@middy/core";
+
+import { logRequest, handleError } from "../middleware/index.js";
 import productsRepository from "../shared/productsRepository.js";
 
-export default async function getProductsList(event) {
-  try {
-    const products = await productsRepository.getProducts();
+async function getProductsListHandler(event) {
+  const products = await productsRepository.getProducts();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(products),
-      headers: {
-        "content-type": "application/json",
-      },
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error.message,
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products),
+    headers: {
+      "content-type": "application/json",
+    },
+  };
 }
+
+const getProductsList = middy(getProductsListHandler);
+getProductsList.use(logRequest()).use(handleError());
+
+export default getProductsList;
