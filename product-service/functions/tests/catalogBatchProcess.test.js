8baf0179c5eb3c12a5bb45ee8d0516df96be022d
Elena Kim <elena.kim3011@gmail.com>
2023-03-21 23:43:33 +0100

    task 6: unit testing for 'catalogBatchProcess' [WIP]

---
+++ product-service/functions/tests/catalogBatchProcess.test.js
@@ -0,0 +1,13 @@
+describe("catalogBatchProcess function", () => {
+  it.todo("should store every product from 'Records' property");
+
+  it.todo("should send SNS message after successfully adding products");
+
+  it.todo(
+    "should send SNS message with company = LEGO if any of the product is LEGO"
+  );
+
+  it.todo(
+    "should send SNS message with company = company of first product, if no LEGO was added"
+  );
+});
