4051e7257f13029931aa704f1c53b80cf0af3431
Elena Kim <elena.kim3011@gmail.com>
2023-03-21 22:38:13 +0100

    task 6: send message to SNS topic

--- product-service/functions/catalogBatchProcess.js
+++ product-service/functions/catalogBatchProcess.js
@@ -3,6 +3,7 @@ import middy from "@middy/core";
 import { logRequest } from "../middleware/index.js";
 import productsRepository from "../shared/productsRepository.js";
 import addProductSchemaValidator from "../schema-validators/addProductSchemaValidator.js";
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 
 async function addProductFromMessage(message) {
   try {
@@ -33,6 +34,24 @@ async function catalogBatchProcessHandler(event) {
   );
 
   console.log(`Successfully added ${result.filter(Boolean).length} products`);
+
+  await sendSNSMessage(result.filter(Boolean));
+
+  console.log("Successfully sent SNS message");
+}
+
+const clientSNS = new SNSClient({ region: process.env.AWS_REGION });
+
+async function sendSNSMessage(products) {
+  const command = new PublishCommand({
+    TopicArn: process.env.CREATE_PRODUCT_TOPIC_ARN,
+    Subject: "New products were added",
+    Message: `New products were added:
+
+${JSON.stringify(products, null, "  ")}`,
+  });
+
+  await clientSNS.send(command);
 }
 
 const catalogBatchProcess = middy(catalogBatchProcessHandler);
