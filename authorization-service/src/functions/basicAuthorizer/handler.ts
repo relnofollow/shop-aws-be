cb8b6ca4a8a65f2e1c3745cf27850b4d66d1670d
Elena Kim <elena.kim3011@gmail.com>
2023-03-23 15:21:15 +0100

    task 7: add 'basicAuthorizer' Lambda function

---
+++ authorization-service/src/functions/basicAuthorizer/handler.ts
@@ -0,0 +1,75 @@
+import {
+  APIGatewayTokenAuthorizerEvent,
+  AuthResponse,
+  PolicyDocument,
+  Statement,
+} from "aws-lambda";
+
+enum Effect {
+  ALLOW = "Allow",
+  DENY = "Deny",
+}
+
+function authorize(authorizationToken: string): [boolean, string] {
+  const [, base64Authorization] = authorizationToken.split(" ");
+
+  const decoded = Buffer.from(base64Authorization, "base64").toString("ascii");
+
+  const [username, password] = decoded.split(":");
+
+  if (process.env[username] && process.env[username] === password) {
+    return [true, username];
+  }
+
+  return [false, null];
+}
+
+function generatePolicy(
+  principalId: string = "",
+  effect: Effect,
+  resource: string
+): AuthResponse {
+  const authResponse: AuthResponse = <AuthResponse>{};
+
+  authResponse.principalId = principalId;
+
+  if (effect && resource) {
+    const policyDocument: PolicyDocument = <PolicyDocument>{};
+
+    policyDocument.Version = "2012-10-17";
+    policyDocument.Statement = [];
+
+    const statementOne: any = {};
+    statementOne.Action = "execute-api:Invoke";
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+
+    policyDocument.Statement[0] = statementOne;
+
+    authResponse.policyDocument = policyDocument;
+  }
+
+  return authResponse;
+}
+
+const basicAuthorizer = async (event: APIGatewayTokenAuthorizerEvent) => {
+  if (event.type !== "TOKEN") {
+    return 401;
+  }
+
+  const { authorizationToken } = event;
+
+  if (!authorizationToken) {
+    return "Unauthorized";
+  }
+
+  const [isAuthorized, user] = authorize(authorizationToken);
+
+  return generatePolicy(
+    user,
+    isAuthorized ? Effect.ALLOW : Effect.DENY,
+    event.methodArn
+  );
+};
+
+export { basicAuthorizer as main };
